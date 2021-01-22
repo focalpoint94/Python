9939964eb7b37e8ff17d53a2c68107e73c931575
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2021-01-22 15:38:02 +0900

    Create kruskal.py

---
+++ kruskal.py
@@ -0,0 +1,51 @@
+
+"""
+# Inputs
+7 9
+1 2 29
+1 5 75
+2 3 35
+2 6 34
+3 4 7
+4 6 23
+4 7 13
+5 6 53
+6 7 25
+
+"""
+
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+
+edges = []
+result = 0
+
+for i in range(1, v+1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+def find_parent(parent, x):
+    if parent[x] == x:
+        return x
+    parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+for edge in edges:
+    if find_parent(parent, edge[1]) != find_parent(parent, edge[2]):
+        result += edge[0]
+        union(parent, edge[1], edge[2])
+
+print(result)
