43eb0f2f8ef8139640b47dab4ef6926c26dd03ac
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2021-02-01 20:52:51 +0900

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,15 +1,16 @@
 
+
 nums = list(map(int, input()))
 
 result = 0
 
-if nums[0] != 0 and nums[1] != 0:
+if nums[0] != 0 and nums[1] != 0 and nums[0] != 1 and nums[1] != 1:
     result += nums[0] * nums[1]
 else:
     result += nums[0] + nums[1]
 
 for num in nums[2:]:
-    if num == 0:
+    if num == 0 or num == 1:
         result += num
     else:
         result *= num
